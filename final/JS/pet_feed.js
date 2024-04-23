7748e79721ca8e2fb242be7c69da2f47cc54f009
Sarah Massoudi <sarahmassoudi@10-18-186-30.dynapool.wireless.nyu.edu>
2024-04-23 16:08:54 -0400

    Pushing work on the final, basic semantic tags, styling, and basic JS

---
+++ final/JS/pet_feed.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const pet_btn = document.getElementById('pet');
+    const feed_btn = document.getElementById('treat');
+
+
+    const hap_bar = document.getElementById('hap-bar');
+    const hunger_bar = document.getElementById('hunger-bar');
+
+    pet_btn.addEventListener('click', function() {
+
+    });
+    
+
+});
