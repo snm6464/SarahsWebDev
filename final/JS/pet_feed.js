fa5ad885ac5fb6933ebb3cf911959df4b44c55f3
Sarah Massoudi <sarahmassoudi@Sarahs-Air.lan>
2024-04-28 13:00:27 -0400

    Pushing progress on final - JS is working with minor bugs and media queries added

--- final/JS/pet_feed.js
+++ final/JS/pet_feed.js
@@ -1,14 +1,79 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const pet_btn = document.getElementById('pet');
-    const feed_btn = document.getElementById('treat');
+    var pet_btn = document.getElementById('pet');
+    var feed_btn = document.getElementById('treat');
 
 
-    const hap_bar = document.getElementById('hap-bar');
-    const hunger_bar = document.getElementById('hunger-bar');
+    var hap_bar = document.getElementById('hap-bar');
+    var hunger_bar = document.getElementById('hunger-bar');
+
+    
+    //check if hunger_bar style width exists, if it doesn't make it 100
+    //var happiness = hap_bar.style.width;
+
+    if (!hunger_bar.style.width) {
+        hunger_bar.style.width = "0%";
+    } 
+    var hunger = Math.floor(hunger_bar.style.width.split("%")[0]);
+
+    if (!hap_bar.style.width) {
+        hap_bar.style.width = "0%";
+    }
+
+    var happiness = Math.floor(hap_bar.style.width.split("%")[0]);
+
+
+    console.log("hello", hunger);
+    console.log("hello", happiness);
+
+    //var width_ratio = 
 
     pet_btn.addEventListener('click', function() {
+        //console.log("clicked");
+        happiness = Math.floor(hap_bar.style.width.split("%")[0]);
+        if (!(happiness >= 100)) {
+            console.log('in if');
+            
+            happiness += 10;
+            hap_bar.style.width = `${happiness}%`
+
+            // is this correct syntax???
+        }
+    });
+
+    feed_btn.addEventListener('click', function() {
+        hunger = Math.floor(hunger_bar.style.width.split('%')[0]);
+        
+        if (!(hunger >= 100)) {
+            console.log('hello');
+            
+            hunger += 10;
+            hunger_bar.style.width = `${hunger}%`
+        }
 
     });
+
+    function depleteHunger() {
+        hunger = Math.floor(hunger_bar.style.width.split('%')[0]);
+        
+        if(hunger >= 0) {
+            hunger -= 10;
+            hunger_bar.style.width = `${hunger}%`;
+        };
+    };
+
+    function depleteHappiness() {
+        happiness = Math.floor(hap_bar.style.width.split('%')[0])
+        
+        if (happiness >0) {
+            happiness -= 10;
+            hap_bar.style.width = `${happiness}%`;
+        };
+    };
+
+    // doesn't work????
+    setInterval(depleteHunger(), 10);
+    setInterval(depleteHappiness(),10);
+
     
 
 });
